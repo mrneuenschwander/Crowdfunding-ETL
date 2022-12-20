2a124dd6c898d321bf1a59e0e82f5d5e9b0bcbe1
mrneuenschwander <116296092+mrneuenschwander@users.noreply.github.com>
2022-12-19 19:10:40 -0700

    getting ahead

---
+++ Challenge Code/SQL_Analysis_starter_code.sql
@@ -0,0 +1,31 @@
+-- Challenge Bonus queries.
+-- 1. (2.5 pts)
+-- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
+
+
+
+-- 2. (2.5 pts)
+-- Using the "backers" table confirm the results in the first query.
+
+
+
+-- 3. (5 pts)
+-- Create a table that has the first and last name, and email address of each contact.
+-- and the amount left to reach the goal for all "live" projects in descending order. 
+
+
+
+
+-- Check the table
+
+
+-- 4. (5 pts)
+-- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
+-- and has the first and last name of each backer, the cf_id, company name, description, 
+-- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+
+
+
+-- Check the table
+
+
